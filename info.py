72da1bda2c2505b5a473ceec75d7bc6622294120
dragonlin <509730796@qq.com>
2020-04-17 11:11:17 +0800

    去掉敏感的个人信息

--- info.py
+++ info.py
@@ -1,38 +1,34 @@
-# pakge_name = 'com.ChillyRoom.DungeonShooter'
-# apk_name = 'TapTap-2.6.1.apk'
-# oppo_info = {'pw': 'a18680340380z'}
-
 akp_info = {
     'tap': {
-            'apk_name': 'xxxxxxx',
-            'pakge_name': 'xxxxr'
+            'apk_name': 'xxxxxx',
+            'pakge_name': 'xxxxxx'
             }
 }
 
 phone_info = {
     'oppo': {
-        'pw': 'a18680340380z'
+        'pw': 'xxxxxx'
     }
 }
 
 user_info = {
     'account1': {
-        'account':  'xxxxx', 'pw': 'xxxx'
+        'account':  'xxxxxxxxxxxx', 'pw': 'xxxxxx'
     },
     'account2': {
-        'account':  'xxxx', 'pw': 'xxxxx'
+        'account':  'xxxxxxxxxxxx', 'pw': 'xxxxxx'
         }
 
 }
 
 devices_info = {
     'oppo_A83': 'xxxxxx',
-    'oppo_A5': 'xxxxxxx'
+    'oppo_A5': 'xxxxxx'
 }
 
 
 run_info = {
     # [device, account, apk, phone]
     1: ['oppo_A83', 'account1', 'tap', 'oppo'],
-    2: ['oppo_A5', 'account2', 'tap', 'oppo']
+    2: ['oppo_A83', 'account1', 'tap', 'oppo']
 }
