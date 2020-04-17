7f3bb23e4654849b7fd4ce10e6ff27d7271b375f
dragonlin <509730796@qq.com>
2020-04-17 10:54:50 +0800

    一些配置信息

---
+++ info.py
@@ -0,0 +1,38 @@
+# pakge_name = 'com.ChillyRoom.DungeonShooter'
+# apk_name = 'TapTap-2.6.1.apk'
+# oppo_info = {'pw': 'a18680340380z'}
+
+akp_info = {
+    'tap': {
+            'apk_name': 'TapTap-2.6.1.apk',
+            'pakge_name': 'com.ChillyRoom.DungeonShooter'
+            }
+}
+
+phone_info = {
+    'oppo': {
+        'pw': 'a18680340380z'
+    }
+}
+
+user_info = {
+    'account1': {
+        'account':  '509730796@qq.com', 'pw': '18680340380'
+    },
+    'account2': {
+        'account':  '421352467@qq.com', 'pw': '18680340380'
+        }
+
+}
+
+devices_info = {
+    'oppo_A83': '5PZTCUOJZTJFEM5S',
+    'oppo_A5': '86248021'
+}
+
+
+run_info = {
+    # [device, account, apk, phone]
+    1: ['oppo_A83', 'account1', 'tap', 'oppo'],
+    2: ['oppo_A5', 'account2', 'tap', 'oppo']
+}
