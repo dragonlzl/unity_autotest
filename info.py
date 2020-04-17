6003d2bf6d777239173571681d6dc3ea116a71c7
dragonlin <509730796@qq.com>
2020-04-17 11:08:53 +0800

    去掉敏感的个人信息

--- info.py
+++ info.py
@@ -4,8 +4,8 @@
 
 akp_info = {
     'tap': {
-            'apk_name': 'TapTap-2.6.1.apk',
-            'pakge_name': 'com.ChillyRoom.DungeonShooter'
+            'apk_name': 'xxxxxxx',
+            'pakge_name': 'xxxxr'
             }
 }
 
@@ -17,17 +17,17 @@ phone_info = {
 
 user_info = {
     'account1': {
-        'account':  '509730796@qq.com', 'pw': '18680340380'
+        'account':  'xxxxx', 'pw': 'xxxx'
     },
     'account2': {
-        'account':  '421352467@qq.com', 'pw': '18680340380'
+        'account':  'xxxx', 'pw': 'xxxxx'
         }
 
 }
 
 devices_info = {
-    'oppo_A83': '5PZTCUOJZTJFEM5S',
-    'oppo_A5': '86248021'
+    'oppo_A83': 'xxxxxx',
+    'oppo_A5': 'xxxxxxx'
 }
 
 
